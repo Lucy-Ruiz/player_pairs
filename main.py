ba88ed696c18fede8a6efd0e117fb2f503e8bfec
Lucy Ruiz <lucyruiz846@gmail.com>
2022-08-22 14:55:21 -0700

    docs: Completed general algorithm: Description of general steps that to start coding.

---
+++ main.py
@@ -0,0 +1,18 @@
+#Project Pairs Project
+#. import random
+#. define a function to store list
+#. list should be named 'deck'
+#. insert in the list each one of the required elements 
+#. create a for loop and range to execute the cycle four times and get four elements of each
+#. define a function to shuffle list
+#. use random.shuffle(mylist) to mix elements
+#. define a function to give cards to players
+#. create four new lists, one for each player
+#. use a for loop to to give 5 cards to each player
+#. update the shuffled deck after a card has been given to assign only remaining cards and avoid repetition
+#. research how to find duplicated elements in lists for each player 
+#. printto display -> player number, their hand and number of pairs
+#. compare number of pairs of each player. Display if there is a winner with the highest number of pairs or if there are tied players
+#. 5 Functions defined until now, can be more during the process
+#. Functions -> to store deck list, to shuffle list, to give cards to players, to count pairs and display, to compare results and display
+
